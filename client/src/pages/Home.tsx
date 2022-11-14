a6d0d58181b2e88e4e251a452922190a78ee392f
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2022-11-14 22:42:50 +0900

    send cookie to server

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,28 +3,46 @@ import axios from "axios";
 import {MessengerClient} from "../proto/MessangerServiceClientPb"
 import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 import {MessageRequest} from "../proto/messanger_pb";
+import {credentials, Metadata} from "@grpc/grpc-js";
+import {LoginServiceClient} from "../proto/UserServiceClientPb";
+import {LoginRequest} from "../proto/user_pb";
+import {Button} from "@mui/material";
 
 const Home = () => {
     const [a, setA] = useState("")
 
-    useEffect(() => {
-        const client = new MessengerClient('http://localhost:8080')
+    // useEffect(() => {
+    // }, [])
+
+    const Login = () => {
+        const client = new LoginServiceClient('http://localhost:8080')
+        const req = new LoginRequest()
+        req.setAccountName("aaa")
+        req.setPassword("ssss")
+        client.login(req, null).then(r => console.log(r))
+    }
+
+    const Message = () => {
+        const client = new MessengerClient('http://localhost:8080', null, {
+            withCredentials: true
+        })
         const req = new MessageRequest()
         req.setMessage("dddssksk")
         client.createMessage(req, null, res => console.log(res))
-        client.getMessages(new Empty()).on("data", m => {
-            setA(m.getMessage())
-        })
-        // axios.get('http://localhost:9090').then(res => {
-        //     console.log(res)
-        //     setA(res.data)
-        // })
-    }, [])
+        client.getMessages(new Empty())
+            .on("data", m => {
+                setA(m.getMessage())
+            }, )
+    }
+
     return (<>
+        <Button onClick={Login}>ログイン</Button>
+        <Button onClick={Message}>メッセージ</Button>
         {
             a
         }
     </>)
 }
 
-export default Home;
+
+    export default Home;
